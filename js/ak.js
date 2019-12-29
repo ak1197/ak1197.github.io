0b1da14000cbccc0358e6658cc7d620a79eddb1b
ARUNKUMAR S <arunkumar.ak1197@yahoo.com>
2019-12-29 12:11:00 +0530

    Update ak.js

--- js/ak.js
+++ js/ak.js
@@ -1,6 +1,17 @@
-﻿var app = angular.module('akApp', ['$scope']);
-app.controller('ak', function ($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
-    console.log($scope);
+﻿var akApp = angular.module('akApp', []);
+akApp.controller('ak', function ($scope) {
+    $scope.save = function () {
+        console.log($scope.visitorName, $scope.visitorEmail, $scope.visitorComment);
+        Email.send({
+            Host: "smtp.yourisp.com",
+            Username: "username",
+            Password: "password",
+            To: 'them@website.com',
+            From: "you@isp.com",
+            Subject: "This is the subject",
+            Body: "And this is the body"
+        }).then(
+            message => alert(message)
+        );
+    };
 });
