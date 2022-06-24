11db34afd3b51b352c2bb0bd1ccfa93241254f81
ARUNKUMAR S <arunkumar.ak1197@outlook.com>
2022-06-24 15:35:17 +0530

    Experience and certifications added

--- js/ak.js
+++ js/ak.js
@@ -4,11 +4,13 @@ const comp1Start = new Date(2018, 5, 20)
 const comp1End = new Date(2020,9,15)
 const comp2Start = new Date(2020,9,19)
 const comp2End = new Date(2021,11,1)
+const comp3Start = new Date(2021, 11, 6)
 
 
 $(document).ready(function () {
     document.getElementById('txtExperience').innerHTML = getDuration(careerStart)
-    document.getElementById('txtCSSExp').innerHTML = getDuration(comp2Start)
+    document.getElementById('txtFISExp').innerHTML = getDuration(comp3Start)
+    document.getElementById('txtCSSExp').innerHTML = getDuration(comp2Start, comp2End)
     document.getElementById('txtVCXExp').innerHTML = getDuration(comp1Start, comp1End)
 })
 
