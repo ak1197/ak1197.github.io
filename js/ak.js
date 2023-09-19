6c54cd1ee30bbd0a67820739fa64cb94ebfd3e1e
Arunkumar Subbiah <Arunkumar.Subbiah@shell.com>
2023-09-19 17:50:13 +0530

    exp and certification added

--- js/ak.js
+++ js/ak.js
@@ -1,25 +1,45 @@
-﻿
-const careerStart = new Date(2018, 5, 20)
-const comp1Start = new Date(2018, 5, 20)
-const comp1End = new Date(2020,9,15)
-const comp2Start = new Date(2020,9,19)
-const comp2End = new Date(2021,11,1)
-const comp3Start = new Date(2021, 11, 6)
-
+﻿const careerStart = new Date(2018, 5, 20);
+const comp1Start = new Date(2018, 5, 20);
+const comp1End = new Date(2020, 9, 15);
+const comp2Start = new Date(2020, 9, 19);
+const comp2End = new Date(2021, 11, 1);
+const comp3Start = new Date(2021, 11, 6);
+const comp3End = new Date(2022, 11, 31);
+const comp4Start = new Date(2023, 0, 16);
 
 $(document).ready(function () {
-    document.getElementById('txtExperience').innerHTML = getDuration(careerStart)
-    document.getElementById('txtFISExp').innerHTML = getDuration(comp3Start)
-    document.getElementById('txtCSSExp').innerHTML = getDuration(comp2Start, comp2End)
-    document.getElementById('txtVCXExp').innerHTML = getDuration(comp1Start, comp1End)
-})
+  document.getElementById("txtExperience").innerHTML = getDuration(careerStart);
+  document.getElementById("txtDelExp").innerHTML = getDuration(comp4Start);
+  document.getElementById("txtFISExp").innerHTML = getDuration(
+    comp3Start,
+    comp3End
+  );
+  document.getElementById("txtCSSExp").innerHTML = getDuration(
+    comp2Start,
+    comp2End
+  );
+  document.getElementById("txtVCXExp").innerHTML = getDuration(
+    comp1Start,
+    comp1End
+  );
+});
 
-$('#feedbackForm').on('submit', function (e) {
-    e.preventDefault()
-})
+$("#feedbackForm").on("submit", function (e) {
+  e.preventDefault();
+});
 
-
-function getDuration(startDate, endDate = new Date()){
-    let duration = moment.duration(moment(endDate).diff(startDate), 'milliseconds').as('years').toFixed(2)
-    return '(' + duration + 'years)'
+function getDuration(startDate, endDate = new Date()) {
+    let duratinInMs = moment.duration(
+      moment(endDate).diff(startDate),
+      "milliseconds"
+    );
+  let duration = moment
+    .duration(moment(endDate).diff(startDate), "milliseconds")
+    .as("years")
+    .toFixed(1);
+  if (duration < 1) {
+    let duration = duratinInMs.as("months").toFixed(0);
+    return "(" + duration + " months)";
+  }
+  return "(" + duration + " years)";
 }
